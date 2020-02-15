1bcca9142c4f5db05a8379895b1174edcc0ce7ca
Think <1755337994@qq.com>
2020-02-16 06:34:13 +0800

    2

--- src/main/java/com/bolingcavalry/mavendockerplugindemo/controller/Hello.java
+++ src/main/java/com/bolingcavalry/mavendockerplugindemo/controller/Hello.java
@@ -4,6 +4,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.alibaba.fastjson.JSONObject;
 
 @RestController
 public class Hello {
@@ -12,5 +16,16 @@ public class Hello {
     public String sayHello(){
     	//修改返回的字符串的内容
         return "123456. Hello jenkins, " + new Date();
+    }
+	
+	@RequestMapping("/getjson")
+    public String getjson(){
+        Map< String , Object > jsonMap = new HashMap< String , Object>();
+        jsonMap.put("a",1);
+        jsonMap.put("b","");
+        jsonMap.put("c",null);
+        jsonMap.put("d","zhenghuasheng");
+        String str = JSONObject.toJSONString(jsonMap);
+        return str;
     }
 }
